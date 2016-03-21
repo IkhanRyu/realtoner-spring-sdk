b650e985a71c34f448038ea5a204cd6163a9a068
RyuIkHan <rgs21c@hanmail.net>
2016-03-21 21:53:03 +0900

    reflection_package_added

--- src/main/java/net/realtoner/reflect/ReflectUtils.java
+++ src/main/java/net/realtoner/reflect/ReflectUtils.java
@@ -2,6 +2,7 @@ package net.realtoner.reflect;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.NoSuchElementException;
 
 /**
  * @author RyuIkHan
@@ -12,12 +13,16 @@ public class ReflectUtils {
     public static Method getGetterMethod(Class<?> clazz, Field field) throws NoSuchMethodException{
 
         String fieldName = field.getName();
-        String methodName = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+        String methodName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
 
         Method method = clazz.getMethod(methodName);
 
+        if(method == null){
+            throw new NoSuchElementException("There is no getter method for '" + fieldName + "'.");
+        }
+
         if(!method.getReturnType().equals(field.getType()))
-            throw new NoSuchMethodException("field's return type is " + field.getType().getName() + " , " +
+            throw new NoSuchMethodException("The type of given field is " + field.getType().getName() + " , " +
                     "but getter method's return type is " + method.getReturnType().getName());
 
         return method;
